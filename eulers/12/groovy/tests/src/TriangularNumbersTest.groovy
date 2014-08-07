8c81a039a978accac7282d49a94d380c0cc23e61
Josh Duncan <the.josh.duncan@gmail.com>
2014-08-06 22:58:19 -0500

    Euler's 12: groovy: more test conditions for the TriangularNumbers.find function

--- eulers/12/groovy/tests/src/TriangularNumbersTest.groovy
+++ eulers/12/groovy/tests/src/TriangularNumbersTest.groovy
@@ -9,7 +9,15 @@ class TriangularNumbersTest extends GroovyTestCase {
 //    }
 
     void test_find() {
+        assertEquals(1, test_obj.find(1))
+        assertEquals(3, test_obj.find(2))
+        assertEquals(6, test_obj.find(3))
+        assertEquals(10, test_obj.find(4))
         assertEquals(15, test_obj.find(5))
+        assertEquals(21, test_obj.find(6))
+        assertEquals(28, test_obj.find(7))
+        assertEquals(36, test_obj.find(8))
+        assertEquals(45, test_obj.find(9))
     }
 
     void test_up_to() {
@@ -20,5 +28,7 @@ class TriangularNumbersTest extends GroovyTestCase {
         assertEquals([1,3,6,10,15], test_obj.up_to(5).sort())
         assertEquals([1,3,6,10,15,21], test_obj.up_to(6).sort())
         assertEquals([1,3,6,10,15,21,28], test_obj.up_to(7).sort())
+        assertEquals([1,3,6,10,15,21,28,36], test_obj.up_to(8).sort())
+        assertEquals([1,3,6,10,15,21,28,36,45], test_obj.up_to(9).sort())
     }
 }
