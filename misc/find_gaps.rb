aa62d8589c972636a7ebe76874f53d5df27bab3e
Josh Duncan <the.josh.duncan@gmail.com>
2016-02-01 16:39:25 -0600

    find_gaps.rb: dupe detection.

--- misc/find_gaps.rb
+++ misc/find_gaps.rb
@@ -7,12 +7,13 @@
 ids = ARGF.readlines.map { |line| line.to_i }.sort
 
 max = ids.last
-last = ids.first
+last = 0
 ids.each { |id|
   gap = (last..id).to_a
   gap.delete(last)
   gap.delete(id)
   puts "gap: #{gap.length > 1 ? gap : gap.first }" if (gap.length > 0)
+  puts "dupe: #{id}" if last == id
   last = id
 }
 puts "max: #{max}"
