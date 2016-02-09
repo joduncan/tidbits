ccae8126e5c84f580c4c43aa8ad039567f02f99c
Josh Duncan <the.josh.duncan@gmail.com>
2016-02-09 11:36:06 -0600

    diagonal difference warmup problem for hackerrank

--- misc/hackerrank/algorithms/warmup/diagonal_difference/diagonal_difference.d
+++ misc/hackerrank/algorithms/warmup/diagonal_difference/diagonal_difference.d
@@ -1,15 +1,25 @@
 import std.stdio;
-import std.array;
+import std.string;
+import std.range;
+import std.conv;
+import std.math;
 import std.algorithm;
 
 int main(string[] args)
 {
     int array_size = to!int(chomp(stdin.readln()));
+    int values[][] = new int[][](array_size, array_size);
     int primary_sum = 0;
     int secondary_sum = 0;
     foreach(index; iota(array_size))
     {
-        primary_sum += blah[1][1];
-        secondary_sum += blah[1][1];
+        foreach(iter, item; split(chomp(stdin.readln()))[0..array_size].enumerate())
+        {
+            values[index][iter] = to!int(item);
+        }
+        primary_sum += values[index][index];
+        secondary_sum += values[index][array_size-1-index];
     }
+    writeln(abs(primary_sum-secondary_sum));
+    return 0;
 }
