bc40acb05f17f273269c9ed2ecd4ebb1dd2c904c
Josh Duncan <the.josh.duncan@gmail.com>
2016-04-07 11:55:06 -0500

    hackerrank: lisa's workbook: working solution

--- misc/hackerrank/algorithms/implementation/lisas_workbook/lisas_workbook.rb
+++ misc/hackerrank/algorithms/implementation/lisas_workbook/lisas_workbook.rb
@@ -3,13 +3,23 @@
 num_chapters, problems_per_page = ARGF.readline.split.map(&:to_i)
 problems_per_chapter = ARGF.readline.split.map(&:to_i)
 
-current_chapters_first_page_number = 1
+current_chapters_first_page = 0
 special_problems = 0
-(1..num_chapters).each { |ch|
-  (1..problems_per_chapter[ch-1]).each { |problem|
-    special_problems += 1 if ((current_chapters_first_page_number + problem/problems_per_page) == problem)
+(0..num_chapters-1).each { |ch|
+current_chapters_first_page += 1
+  (1..problems_per_chapter[ch]).each { |problem|
+    if ((current_chapters_first_page + (problem-1)/problems_per_page) == problem)
+	  special_problems += 1
+	end
   }
-  current_chapters_first_page_number += problems_per_chapter[ch-1] / problems_per_page
-  current_chapters_first_page_number += 1 if (problems_per_chapter[ch-1] % problems_per_page != 0)
+  next_chapters_first_page = current_chapters_first_page
+  problems_per_chapter[ch] -= problems_per_page
+  if (problems_per_chapter[ch] > 0)
+    next_chapters_first_page += problems_per_chapter[ch] / problems_per_page
+    if ((problems_per_chapter[ch] % problems_per_page) != 0)
+	  next_chapters_first_page += 1
+	end
+  end
+  current_chapters_first_page = next_chapters_first_page
 }
 puts special_problems
