c92a0b3bc7364d9a911f161cee2bf320b7a6590b
Josh Duncan <the.josh.duncan+github@gmail.com>
2016-04-11 22:04:48 -0500

    hackerrank: algos/implementation/library-fine 'corner' case fixed

--- misc/hackerrank/algorithms/implementation/library_fine/library_fine.rb
+++ misc/hackerrank/algorithms/implementation/library_fine/library_fine.rb
@@ -4,10 +4,16 @@ actual = ARGF.readline.split.map(&:to_i)
 expected = ARGF.readline.split.map(&:to_i)
 fines = [ Proc.new { |ad,ed| (ad-ed) * 15 },
           Proc.new { |am,em| (am-em) * 500 },
-          Proc.new { |ay,ey| (ay-ey) == 0 ? 0 : 10000 }
+          Proc.new { |ay,ey| (ay-ey) <= 0 ? 0 : 10000 }
         ]
 
-# getting the max fine only works because the ranges for day, month, year fines do not overlap.
-fine = fines.zip(actual, expected).map { |func, a, e| func.call(a,e) }.max
+actual_joined = actual.reverse.map { |x| (x < 10) ? "%02d" % x : x.to_s }.join.to_i
+expected_joined = expected.reverse.map { |x| (x < 10) ? "%02d" % x : x.to_s }.join.to_i
+fine = 0
+if(actual_joined > expected_joined)
+  # nice idea, but it didn't initially handle situations where the actual return is
+  # before the expected, in a different calendar year.
+  fine = fines.zip(actual, expected).map { |func, a, e| func.call(a,e) }.max
+end
 puts fine
 
