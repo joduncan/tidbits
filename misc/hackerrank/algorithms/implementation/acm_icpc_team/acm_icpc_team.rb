a9a68bb8150531799354d8d8fe466fb4b78f339b
Josh Duncan <the.josh.duncan+github@gmail.com>
2016-04-15 01:23:23 -0500

    hackerrank: algos/implementation/acm-icpc-team faster solution

--- misc/hackerrank/algorithms/implementation/acm_icpc_team/acm_icpc_team.rb
+++ misc/hackerrank/algorithms/implementation/acm_icpc_team/acm_icpc_team.rb
@@ -1,29 +1,35 @@
 #!/bin/ruby
 
 num_presenters, num_topics = ARGF.readline.split.map(&:to_i)
-presenters = {}
+presenters = []
 (0..num_presenters-1).each { |presenter|
-  topics = ARGF.readline.split('').map(&:to_i)
-  presenters[presenter] = topics
+  presenters[presenter] = ARGF.readline.to_i(2)
 }
 
 combos = {}
 
 # ruby has all sorts of whiz-bang helper functions.
-pairs = presenters.keys.permutation(2).map(&:sort).uniq
+pairs = (0..num_presenters-1).to_a.permutation(2).map(&:sort).uniq
+
+max = 0
+max_pairs = 0
 
 pairs.each { |a,b|
-  # zip the topics for each presenter together.
-  pair_topics = presenters[a].zip(presenters[b])
+  # combine the topics of the two presenters
+  known_topics = presenters[a] | presenters[b]
 
   # count how many topics have at least one knowledgeable presenter.
-  count = pair_topics.select { |pair| 1 if pair.any? { |topic| topic == 1 } }.length
+  count =  ("%b" % known_topics).gsub(/0/, '').length
 
-  # populate hash table of # of topics, and how many pairs know that many topics.
-  combos[count] = 0 unless (combos.key?(count))
-  combos[count] += 1
+  if (count >= max)
+    if(count > max)
+      max_pairs = 1
+    else
+      max_pairs += 1
+    end
+    max = count
+  end
 }
 
-max_topics = combos.keys.max
-puts max_topics
-puts combos[max_topics]
+puts max
+puts max_pairs
