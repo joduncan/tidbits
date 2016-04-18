1d2325d895e7f10a3075c9a1b53e945aea371504
Josh Duncan <the.josh.duncan@gmail.com>
2016-04-18 11:22:34 -0500

    hackerrank: algos/implementation/taum-and-bday solution

--- misc/hackerrank/algorithms/implementation/taum_and_bday/taum_and_bday.rb
+++ misc/hackerrank/algorithms/implementation/taum_and_bday/taum_and_bday.rb
@@ -2,9 +2,13 @@
 
 test_cases = ARGF.readline.to_i
 
-(1..test_cases).each { |case|
+(1..test_cases).each { |tc|
   num_black, num_white = ARGF.readline.split.map(&:to_i)
-  black_cost, conversion_cost, white_cost = ARGF.readline.split.map(&:to_i)
+  black_price, white_price, conversion_price = ARGF.readline.split.map(&:to_i)
 
-
+  black_cost = black_price
+  black_cost = (white_price + conversion_price) if(white_price + conversion_price < black_price)
+  white_cost = white_price
+  white_cost = (black_price + conversion_price) if(black_price + conversion_price < white_price)
+  puts num_black * black_cost + num_white * white_cost
 }
