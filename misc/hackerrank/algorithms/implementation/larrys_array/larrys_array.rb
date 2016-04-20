2f887cef4c4ad7c3a69fb457b904ac0c003e1d66
Josh Duncan <the.josh.duncan@gmail.com>
2016-04-20 16:05:50 -0500

    hackerrank: larry's array cleanup. not 'out-of-spec' slow when considering the proper problem space(1000 number maximum vs max 100000)

--- misc/hackerrank/algorithms/implementation/larrys_array/larrys_array.rb
+++ misc/hackerrank/algorithms/implementation/larrys_array/larrys_array.rb
@@ -2,7 +2,6 @@
 
 def rotate(array, first_index)
   first, second, third = array[first_index..first_index+2]
-#  puts "rotating [#{first},#{second},#{third}] of #{array} at index #{first_index}"
   array[first_index..first_index+2] = second, third, first
 end
 
@@ -11,11 +10,8 @@ num_tests = ARGF.readline.to_i
   size=ARGF.readline.to_i
   # consider subtracting 1 from each item to make array indexing easier.
   contents = ARGF.readline.split.map(&:to_i)
-  # front to back sorting doesn't seem to work for at least 1 example
-  # back to front sorting may work, need to iron out how to detect a failure
-  # condition.
+
   (size-1).downto(2) { |final_position|
-    #puts "#{final_position+1}/#{final_position}"
     current_index = contents.index(final_position+1)
 
     # naively choosing triplets: using index-1, index, index+1, rotate twice
@@ -34,21 +30,9 @@ num_tests = ARGF.readline.to_i
       else
         current_index -= 1
         rotate(contents, current_index)
-	# that has the "currently sorting" item in the middle position.
-        #if (current_index == final_position - 1)
-          #rotation_index = current_index
-          #if(final_position > size - 3)
-	    # need to adjust the rotation index for items near the end of the
-	    # sorted list.
-            #rotation_index = size - 3
-          #end
-          #rotate(contents, rotation_index)
-        #end
-        #current_index -= 1
       end
     end
   }
-  #puts contents.join(' ')
   # array should be "sorted" now. see if indices 0-2 are in the correct order.
   result = "YES"
   result = "NO" if contents[0] != 1
